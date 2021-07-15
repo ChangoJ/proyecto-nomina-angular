c80b7103b76489927c98b7650c2d9ba3fa18d10d
Jordan <jordanchango25000@gmail.com>
2021-07-15 11:04:03 -0500

    mejorado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from "./employee/employee.component";
+import { SalaryRuleComponent } from './salary-rule/salary-rule.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { SalaryRuleListComponent } from './salary-rule-list/salary-rule-list.component';
+import { UpdateSalaryRuleComponent } from './update-salary-rule/update-salary-rule.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { CreateSalaryRuleComponent } from './create-salary-rule/create-salary-rule.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'salaryrules', component: SalaryRuleComponent
 
-const routes: Routes = [];
-
+  },
+  {
+    path: 'employees', component: EmployeeComponent
+  },
+  {
+    path: 'createSalaryRules/:ci', component: CreateSalaryRuleComponent
+  },
+  {
+    path: 'employee-list', component: EmployeeListComponent
+  },
+  {
+    path: 'salaryrules-list', component: SalaryRuleListComponent
+  }
+  ,
+  {
+    path: 'updateEmployee/:ci', component: UpdateEmployeeComponent
+  }
+  ,
+  {
+    path: 'updateSalaryRule/:ci', component: UpdateSalaryRuleComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AuthService} from './api/auth.service';
+import {EmployeeService} from './api/employee.service';
+import { EmployeeComponent } from './employee/employee.component';
+import { SalaryRuleComponent } from './salary-rule/salary-rule.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { SalaryRuleListComponent } from './salary-rule-list/salary-rule-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { UpdateSalaryRuleComponent } from './update-salary-rule/update-salary-rule.component';
+import { CreateSalaryRuleComponent } from './create-salary-rule/create-salary-rule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeComponent,
+    SalaryRuleComponent,
+    EmployeeListComponent,
+    FilterPipe,
+    SalaryRuleListComponent,
+    UpdateEmployeeComponent,
+    UpdateSalaryRuleComponent,
+    CreateSalaryRuleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  AuthService,
+  EmployeeService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
